0bb3031a25da5aaae4679e808218049a9d306f2d
Shane da Silva <shane@dasilva.io>
2013-03-03 12:28:46 -0500

    Add SMTP support for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,12 @@ Cloudwatch::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    port: 25,
+    domain: 'cloudwatch.fm',
+  }
 end
