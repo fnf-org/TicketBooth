10d4ea50000e9100b8000a75db96c47963c4a2fa
Shane da Silva <shane@dasilva.io>
2013-03-11 23:56:16 -0700

    Setup email for development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,15 @@ Cloudwatch::Application.configure do
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { host: 'cloudwatch.fm' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: '127.0.0.1',
+    port:    25,
+    domain: 'cloudwatch.fm',
+  }
 end
