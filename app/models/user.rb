0dcc8632019103420f0f7ed39b05f9be1d6a993a
Shane da Silva <shane@dasilva.io>
2013-03-24 00:48:37 -0700

    Add stricter validation of user names
    Adds two before validation hooks to strip surrounding whitespace and
    normalize internal whitespace (e.g. condensing multiple spaces into a
    single space).
    
    Also verify that when a user enters a name that at least two names are
    present. This ensures we don't get users with names that are hard to
    identify (unless they purposely use a fake name, but there's no way we
    can realistically deal with that anyway).

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,16 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
+  before_validation :strip_whitespace, :normalize_spaces
+
   validates_presence_of :name, :email
 
   validates_length_of :name, in: 2..70,
     too_short: 'is too short. Did you forget to enter it?',
     too_long: 'is too long. Perhaps remove middle names?'
+
+  validates_format_of :name, with: /[^\s]+\s[^\s]+(\s[^\s]+)*/i,
+    message: 'must include at least first and last name'
 
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
 
@@ -21,5 +26,16 @@ class User < ActiveRecord::Base
 
   def site_admin?
     SiteAdmin.where(user_id: self).first.present?
+  end
+
+private
+
+  def strip_whitespace
+    self.name.strip
+    self.email.strip
+  end
+
+  def normalize_spaces
+    self.name.gsub!(/\s+/, ' ')
   end
 end
