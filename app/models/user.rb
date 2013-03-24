a948d75af90325ab434b59c0334332e4206cb6b2
Shane da Silva <shane@dasilva.io>
2013-03-24 00:05:53 -0700

    Add support for automatic account locking
    This will protect users from brute-force password attacks.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  # :token_authenticatable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
