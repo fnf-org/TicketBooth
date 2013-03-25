d47f4461e103fb3b1bfc4efe91319920c96e7206
Shane da Silva <shane@dasilva.io>
2013-03-24 22:32:44 -0700

    Redirect to last ticket request on home page
    When a regular user logs in and goes to the home page, they previously
    would have seen the "Coming Soon" page, which isn't terribly
    interesting.
    
    This changes the behaviour to redirect them to see the status of their
    last ticket request, if they have one.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   def index
-    if signed_in? && current_user.site_admin?
-      redirect_to events_path
+    if signed_in?
+      if current_user.site_admin?
+        redirect_to events_path
+      elsif ticket_request = TicketRequest.where(user_id: current_user).last
+        redirect_to event_ticket_request_path(ticket_request.event, ticket_request)
+      end
     end
   end
 end
