63a04c5d67c3d12f66c4c7d87eccf2f66014687e
Shane da Silva <shane@dasilva.io>
2013-04-27 01:23:09 -0700

    Fix mail sending in production
    This has apparently been broken for a while, and the cause is unclear.
    However, we are utilizing SMTP when it's not strictly necessary, so in
    an effort to simplify things we'll try using sendmail instead.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,11 +68,7 @@ Cloudwatch::Application.configure do
   config.action_mailer.default_url_options = { host: 'cloudwatch.fm' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: '127.0.0.1',
-    port:    25,
-    domain: 'cloudwatch.fm',
-    openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,
-  }
+
+  # Send mail to local postfix server
+  config.action_mailer.delivery_method = :sendmail
 end
