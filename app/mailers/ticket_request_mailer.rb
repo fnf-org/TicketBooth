758f67e64d7c0303fc9eeb0340c5310559cefe4e
Shane da Silva <shane@dasilva.io>
2013-05-29 22:57:31 -0700

    Update copy on ticket request emails

--- app/mailers/ticket_request_mailer.rb
+++ app/mailers/ticket_request_mailer.rb
@@ -1,11 +1,13 @@
 class TicketRequestMailer < ActionMailer::Base
+  DEFAULT_SENDER_EMAIL = 'no-reply@cloudwatch.fm'
+
   layout 'email'
-  default from: "no-reply@cloudwatch.fm"
 
   def request_received(ticket_request)
     @ticket_request = ticket_request
     @event = @ticket_request.event
     mail to: "#{@ticket_request.user.name} <#{@ticket_request.user.email}>",
+         from: from_email,
          subject: "#{@event.name} ticket request confirmation"
   end
 
@@ -13,6 +15,13 @@ class TicketRequestMailer < ActionMailer::Base
     @ticket_request = ticket_request
     @event = @ticket_request.event
     mail to: "#{@ticket_request.user.name} <#{@ticket_request.user.email}>",
+         from: from_email,
          subject: "Your #{@event.name} ticket request has been approved!"
+  end
+
+private
+
+  def from_email
+    "#{@event.name} <#{DEFAULT_SENDER_EMAIL}>"
   end
 end
