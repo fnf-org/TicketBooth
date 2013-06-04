98a72c432d541164fa8720aefeb86cd47ef31233
Shane da Silva <shane@dasilva.io>
2013-06-04 01:07:34 -0700

    Change deploy server domain
    Since the application server is now hosted on a different box, we want
    to explicitly specify the host name to deploy to.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application           , 'cloudwatch'
 set :user                  , application
-set :domain                , 'cloudwatch.fm'
+set :domain                , 'zeno.cloudwatch.fm'
 set :use_sudo              , false
 set :ssh_options           , { forward_agent: true, compression: 'none' }
 set :keep_releases         , 20 # Keep the last N releases
