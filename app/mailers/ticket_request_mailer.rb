ad1340c66e2b1c6404e4e43c5b7e702130b27002
Shane da Silva <shane@dasilva.io>
2013-06-08 14:15:07 -0700

    Change request approved email for free tickets
    The request approved email didn't make sense for users with free
    tickets, as they didn't need to pay anything. This has been changed to
    make more sense.

--- app/mailers/ticket_request_mailer.rb
+++ app/mailers/ticket_request_mailer.rb
@@ -6,7 +6,7 @@ class TicketRequestMailer < ActionMailer::Base
   def request_received(ticket_request)
     @ticket_request = ticket_request
     @event = @ticket_request.event
-    mail to: "#{@ticket_request.user.name} <#{@ticket_request.user.email}>",
+    mail to: to_email,
          from: from_email,
          subject: "#{@event.name} ticket request confirmation"
   end
@@ -14,12 +14,16 @@ class TicketRequestMailer < ActionMailer::Base
   def request_approved(ticket_request)
     @ticket_request = ticket_request
     @event = @ticket_request.event
-    mail to: "#{@ticket_request.user.name} <#{@ticket_request.user.email}>",
+    mail to: to_email,
          from: from_email,
          subject: "Your #{@event.name} ticket request has been approved!"
   end
 
 private
+
+  def to_email
+    "#{@ticket_request.user.name} <#{@ticket_request.user.email}>"
+  end
 
   def from_email
     "#{@event.name} <#{DEFAULT_SENDER_EMAIL}>"
--- spec/mailers/ticket_request_mailer_spec.rb
+++ spec/mailers/ticket_request_mailer_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 describe TicketRequestMailer do
   let(:user) { User.make! }
   let(:event) { Event.make! name: 'Test Event' }
-  let(:ticket_request) { TicketRequest.make! event: event, user: user }
+  let(:price) { nil }
+
+  let(:ticket_request) do
+    TicketRequest.make! event: event,
+                        user: user,
+                        special_price: price
+  end
 
   describe '#request_received' do
     let(:mail) { described_class.request_received(ticket_request) }
@@ -26,6 +32,47 @@ describe TicketRequestMailer do
 
     it "includes a link to the user's ticket request" do
       mail.body.encoded.should match(event_ticket_request_url(event, ticket_request))
+    end
+  end
+
+  describe '#request_approved' do
+    let(:mail) { described_class.request_approved(ticket_request) }
+    let(:body) { mail.body.encoded }
+
+    it 'renders the subject' do
+      mail.subject.should == 'Your Test Event ticket request has been approved!'
+    end
+
+    it 'renders the receiver email' do
+      mail.to.should == [user.email]
+    end
+
+    it "includes the user's name" do
+      body.should match(user.first_name)
+    end
+
+    it "includes the event's name" do
+      body.should match('Test Event')
+    end
+
+    it "includes a link to the volunteer sign-up page" do
+      mail.body.encoded.should match(event_shifts_url(event))
+    end
+
+    context 'when the ticket request is free' do
+      let(:price) { 0 }
+
+      it 'does not include the word "purchase"' do
+        body.should_not match('purchase')
+      end
+    end
+
+    context 'when the ticket request is not free' do
+      let(:price) { 10 }
+
+      it 'includes the word "purchase"' do
+        body.should match('purchase')
+      end
     end
   end
 end
