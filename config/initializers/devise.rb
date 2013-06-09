7fc4bd319dbfbbb3174c69f7c6093d52da9a4f7f
Shane da Silva <shane@dasilva.io>
2013-06-08 19:46:42 -0700

    Change email address of Devise emails
    Now that we can receive emails to arbitrary `@cloudwatch.fm` addresses,
    change the email address to be from `support@cloudwatch.fm`.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = 'no-reply@cloudwatch.fm'
+  config.mailer_sender = 'CloudWatch <support@cloudwatch.fm>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
