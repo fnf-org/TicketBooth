fnf-org/TicketBooth

commit 19d403685fe136b3e59b942f20644d04ce33e389

Fix broken ticket request spec
Commit 319cf6d7 removed a validation that ensured the existence of a
user for a ticket request. That commit removed the uniqueness constraint
of users being limited to one ticket request per event.

The commit didn't mention the removal of this existence validation, so I
think it was done in error. I've added it back in and fixed a spec that
was was broken in the process.

app/models/ticket_request.rb
@@ -18,7 +18,7 @@ class TicketRequest < ActiveRecord::Base
 
   accepts_nested_attributes_for :user
 
-  validates :user, presence: true
+  validates :user, presence: true, existence: true
 
   validates :event_id, presence: true, existence: true
 

spec/models/ticket_request_spec.rb
@@ -18,17 +18,12 @@ describe TicketRequest do
 
       context 'when user exists' do
         let(:user) { User.make! }
-
         it { should be_valid }
       end
 
       context 'when user does not exist' do
         let(:user) { User.make! }
-
-        before do
-          user.delete
-        end
-
+        before { user.delete }
         it { should_not be_valid }
       end
 
@@ -42,7 +37,9 @@ describe TicketRequest do
             TicketRequest.make! event: event, user: user
           end
 
-          it { should_not be_valid }
+          # TODO: Decide whether we would rather allow editing of ticket
+          # requests instead of making multiple requests
+          it { should be_valid }
         end
 
         context 'and they have created requests only for other events' do

commit 2488d6eb63b333ac8e128918877ad31f1812ea8f

Don't check for existence of new user
Commit 19d40368 broke the ticket request flow for new users. This was
due to the fact that the existence validation for users of a ticket
requests was trying to find an obviously non-existent user.

The solution is to not run the validation if the associated user is
detected to be a new record.

app/models/ticket_request.rb
@@ -18,7 +18,7 @@ class TicketRequest < ActiveRecord::Base
 
   accepts_nested_attributes_for :user
 
-  validates :user, presence: true, existence: true
+  validates :user, presence: true, existence: { unless: -> { user.try(:new_record?) } }
 
   validates :event_id, presence: true, existence: true
 

spec/models/ticket_request_spec.rb
@@ -27,6 +27,11 @@ describe TicketRequest do
         it { should_not be_valid }
       end
 
+      context 'when user is new' do
+        let(:user) { User.make }
+        it { should be_valid }
+      end
+
       context 'when the user has other ticket requests' do
         let(:event) { Event.make! }
         let(:user) { User.make! }