efbdba4d001c14f575ec7052b0769fc1ee45a125
Shane da Silva <shane@dasilva.io>
2013-08-04 14:37:30 -0700

    Render views in all controller specs
    This will allow us to write simpler controller specs while still having
    some of the safety checks provided by view specs, without having to
    write view specs.
    
    The test suite will be slowed down slightly, but that's an acceptable
    trade-off for ensuring correctness.

---
+++ spec/support/render_views.rb
@@ -0,0 +1,7 @@
+# Since we don't exhaustively write view specs, make sure our controller specs
+# always render the views (even though we don't check this output). This way
+# we'll catch dumb rendering bugs even with controller specs, allowing us to
+# write fewer view specs.
+RSpec.configure do |config|
+  config.render_views
+end
