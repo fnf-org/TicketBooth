a56e38de59f2cd4458b7e98e89a727570d0f4008
Shane da Silva <shane@dasilva.io>
2013-08-04 17:41:02 -0700

    Use explict asset compression configuration
    In preparation for Rails 4, set these explicitly.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,8 +11,9 @@ Cloudwatch::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+  # Compress CSS and JavaScripts
+  config.assets.css_compressor = :sass
+  config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
