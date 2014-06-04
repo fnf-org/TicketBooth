9c51f713c7b98aab4f0e865489fd5a2e2591bab5
Shane da Silva <shane@dasilva.io>
2014-06-04 11:54:12 -0700

    Update Devise sender to HelpingCulture

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = 'CloudWatch <support@helpingculture.com>'
+  config.mailer_sender = 'Helping Culture <support@helpingculture.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
