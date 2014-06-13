8ffaea616bd053fa8cbbd2acc3dd9554aed6597a
Shane da Silva <shane@dasilva.io>
2014-06-13 10:34:15 -0700

    Add vanity routes for FnF ticket sales

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Cloudwatch::Application.routes.draw do
 
   devise_for :users
 
+  get 'fnf-tickets', controller: :ticket_requests, action: :new, event_id: 1
+  get 'fnf_tickets', controller: :ticket_requests, action: :new, event_id: 1
+  get 'fnftickets', controller: :ticket_requests, action: :new, event_id: 1
+  get 'fnf', controller: :ticket_requests, action: :new, event_id: 1
+
   resources :payments, only: [:new, :create, :show] do
     collection do
       get :other
