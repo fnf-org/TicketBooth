2ebdde6ffe59817e780bdf45895310680cc12873
Shane da Silva <shane@dasilva.io>
2015-06-02 22:51:08 -0700

    Rename serve_static_assets -> serve_static_files
    This silences a deprecation warning.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Cloudwatch::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress CSS and JavaScripts
   config.assets.css_compressor = :sass
