3f18bf59b5e62714ee216c9e3667296a48ce5b7b
Shane da Silva <shane@dasilva.io>
2015-06-03 00:24:09 -0700

    Silence forwarded emails
    It isn't clear where these messages are coming from and they're chewing
    through our send quotas. Stop them by simply not sending an email for
    now.

--- app/mailers/mandrill_mailer.rb
+++ app/mailers/mandrill_mailer.rb
@@ -1,11 +1,12 @@
 class MandrillMailer < ActionMailer::Base
   def inbound_email(message)
-    mail to: 'shane@dasilva.io',
-         from: "#{message['from_name']} <forward@helpingculture.com>",
-         reply_to: "#{message['from_name']} <#{message['from_email']}>",
-         subject: message['subject'] do |format|
-      format.text { render text: message['text'] }
-      format.html { render text: message['html'] }
-    end
+    nil # For now, don't forward messages as it's all spam
+    #mail to: 'shane@dasilva.io',
+         #from: "#{message['from_name']} <forward@helpingculture.com>",
+         #reply_to: "#{message['from_name']} <#{message['from_email']}>",
+         #subject: message['subject'] do |format|
+      #format.text { render text: message['text'] }
+      #format.html { render text: message['html'] }
+    #end
   end
 end
