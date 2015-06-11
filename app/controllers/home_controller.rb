1d6188703146ff1998f38bae5ea9e03689f7e64c
Shane da Silva <shane@dasilva.io>
2015-06-11 00:45:01 -0700

    Display current event when logging in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,12 @@ class HomeController < ApplicationController
     if signed_in?
       if current_user.site_admin? || current_user.event_admin?
         redirect_to events_path
-      elsif ticket_request = TicketRequest.where(user_id: current_user).last
+      elsif ticket_request = TicketRequest.where(user_id: current_user)
+                                          .where(event_id: 3).last # TODO: Need something better
         redirect_to event_ticket_request_path(ticket_request.event, ticket_request)
+      else
+        # TODO: Figure out some clever approach for this
+        redirect_to new_event_ticket_request_path(event_id: 3)
       end
     end
   end
