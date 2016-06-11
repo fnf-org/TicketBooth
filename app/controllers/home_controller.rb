1d6d879a3734f55fd1ba1a8f6a920e6230847143
Shane da Silva <shane@dasilva.io>
2016-06-11 16:09:20 -0700

    Show ticket requests for most recent event

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,15 @@
 class HomeController < ApplicationController
   def index
     if signed_in?
+      most_recent_event = Event.order(id: :desc).first
       if current_user.site_admin? || current_user.event_admin?
         redirect_to events_path
       elsif ticket_request = TicketRequest.where(user_id: current_user)
-                                          .where(event_id: 3).last # TODO: Need something better
+                                          .where(event: most_recent_event).last
         redirect_to event_ticket_request_path(ticket_request.event, ticket_request)
       else
-        # TODO: Figure out some clever approach for this
-        redirect_to new_event_ticket_request_path(event_id: 3)
+        most_recent_event = Event.order(id: :desc).first
+        redirect_to new_event_ticket_request_path(event: most_recent_event)
       end
     end
   end
