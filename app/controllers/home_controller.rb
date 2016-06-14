6e56980d073700e8aa9bd6c4b2a9f2a7bc003822
Shane da Silva <shane@dasilva.io>
2016-06-13 22:02:09 -0700

    Fix home page for non-site admins

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,11 @@ class HomeController < ApplicationController
       if current_user.site_admin? || current_user.event_admin?
         redirect_to events_path
       elsif ticket_request = TicketRequest.where(user_id: current_user)
-                                          .where(event: most_recent_event).last
+                                          .where(event_id: most_recent_event.id).last
         redirect_to event_ticket_request_path(ticket_request.event, ticket_request)
       else
         most_recent_event = Event.order(id: :desc).first
-        redirect_to new_event_ticket_request_path(event: most_recent_event)
+        redirect_to new_event_ticket_request_path(event_id: most_recent_event.id)
       end
     end
   end
