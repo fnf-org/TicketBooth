e1852c377d43e147595a126fc2bda8ac39a25be1
Shane da Silva <shane@dasilva.io>
2017-06-09 22:06:16 -0700

    Remove deprecation warning about transaction callbacks
    After upgrading to Rails 4.2 we started seeing this warning. Fix it by
    adding this setting.

--- config/application.rb
+++ config/application.rb
@@ -51,6 +51,8 @@ module Cloudwatch
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    config.active_record.raise_in_transactional_callbacks = true
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
