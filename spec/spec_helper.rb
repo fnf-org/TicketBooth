7e93ed29f0dd28a4fdc694b28d244bc61a857d8f
Shane da Silva <shane@dasilva.io>
2017-06-09 22:38:44 -0700

    Fix deprecation warnings for RSpec `should` syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each { |f| require f }
+
+RSpec.configure do |config|
+  config.expect_with(:rspec) do |c|
+    c.syntax = [:should, :expect]
+  end
+end
