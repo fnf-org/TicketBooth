e0f96aa9f7e7f6ce8226e50b06f5da5a89da59e8
Shane da Silva <shane@dasilva.io>
2017-06-09 23:14:11 -0700

    Disable fees
    For now, we don't want to charge extra to compensate for the fees, and
    are willing to incur those costs ourselves.

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -4,8 +4,10 @@ module PaymentsHelper
   STRIPE_RATE = BigDecimal.new('0.029', 10) # 2.9% per transaction
   STRIPE_TRANSACTION_FEE = BigDecimal.new('0.30', 10) # +30 cents per transaction
   def extra_amount_to_charge(original_amount)
-    extra = (original_amount * STRIPE_RATE + STRIPE_TRANSACTION_FEE) / (1 - STRIPE_RATE)
-    extra_cents = (extra * 100).ceil # Round up to the nearest cent
-    BigDecimal.new(extra_cents, 10) / 100
+    # XXX: For now, disable passing fees on to user
+    # extra = (original_amount * STRIPE_RATE + STRIPE_TRANSACTION_FEE) / (1 - STRIPE_RATE)
+    # extra_cents = (extra * 100).ceil # Round up to the nearest cent
+    # BigDecimal.new(extra_cents, 10) / 100
+    0
   end
 end
