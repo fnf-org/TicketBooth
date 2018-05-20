fnf-org/TicketBooth

commit ad83a7722434ca8a261b6b5739cb4474481cbce0

Update most gems
This commit started off as a removal of some gem constraints and running
a `bundle update`, and was slowly scoped back to re-include some
gem version constraints.

We also made some changes to fix deprecation warnings and other errors
introduced by upgrading some gems.

app/uploaders/photo_uploader.rb
@@ -1,5 +1,4 @@
 class PhotoUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MimeTypes
   include CarrierWave::MiniMagick
 
   process :set_content_type

Gemfile
@@ -1,30 +1,27 @@
 source 'https://rubygems.org'
 
-gem 'rails', '~> 4.2'
+gem 'rails'
 
 gem 'attribute_normalizer'
 gem 'carrierwave'
 gem 'coffee-rails'
 gem 'commonjs'
 gem 'country_select'
-gem 'devise', '~> 3.5'
+gem 'devise'
 gem 'haml'
 gem 'jquery-rails'
 gem 'less'
 gem 'less-rails'
 gem 'libv8'
 gem 'mini_magick'
-gem 'pg'
+gem 'pg', '~> 0.18'
 gem 'protected_attributes'
-gem 'rbnacl'
-gem 'rbnacl-libsodium', require: 'rbnacl/libsodium'
-gem 'bcrypt_pbkdf'
 gem 'sass-rails'
 gem 'sentry-raven'
 gem 'stripe'
 gem 'therubyracer', platforms: :ruby
 gem 'thin'
-gem 'twitter-bootstrap-rails'
+gem 'twitter-bootstrap-rails', '~> 2.2.7'
 gem 'uglifier'
 gem 'unicorn'
 gem 'validates_existence'

config/application.rb
@@ -45,12 +45,6 @@ module Cloudwatch
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
 
-    # Enforce whitelist mode for mass assignment.
-    # This will create an empty whitelist of attributes available for mass-assignment for all models
-    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
-
     config.active_record.raise_in_transactional_callbacks = true
 
     # Enable the asset pipeline

config/initializers/haml.rb
@@ -1,1 +1,0 @@
-Haml::Template.options[:ugly] = true

spec/support/devise.rb
@@ -1,3 +1,3 @@
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end

commit 51b5a3f5fa4228474942980182ab16c0d64cd320

Fix Carrierwave uploads
Gem update in ad83a772 broke uploads.

app/uploaders/photo_uploader.rb
@@ -1,8 +1,6 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process :set_content_type
-
   # Prevent enormous photos from being stored
   process resize_to_limit: [1000, 1000]
 