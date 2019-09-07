c6f1eb2fc70d07a030b1929271dc4b8aa4333eff
Shane da Silva <shane@dasilva.io>
2019-09-07 01:21:07 -0700

    Change ticket max for Fall Classic campout to 4
    We want the max tickets to be 4 regardless of role.

--- app/models/ticket_request.rb
+++ app/models/ticket_request.rb
@@ -12,11 +12,11 @@ class TicketRequest < ActiveRecord::Base
   ]
 
   TICKET_LIMITS = {
-    (ROLE_UBER_COORDINATOR = 'uber_coordinator') => 6,
-    (ROLE_COORDINATOR = 'coordinator') => 6,
+    (ROLE_UBER_COORDINATOR = 'uber_coordinator') => 4,
+    (ROLE_COORDINATOR = 'coordinator') => 4,
     (ROLE_CONTRIBUTOR = 'contributor') => 4,
-    (ROLE_VOLUNTEER = 'volunteer') => 2,
-    (ROLE_OTHER = 'other') => 2,
+    (ROLE_VOLUNTEER = 'volunteer') => 4,
+    (ROLE_OTHER = 'other') => 4,
   }
 
   ROLES = {
