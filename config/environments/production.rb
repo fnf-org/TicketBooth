d4ea58e4022db4af640fecd8d7499479b0b684c5
Mike Matera <maximus@fatboycentral.com>
2022-04-24 13:22:54 -0700

    Don't need to compile assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,9 +21,7 @@ TicketBooth::Application.configure do
   config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  ### MM: Modified for Kubernetes
-  ## FIXME: Why doe we need this if we compile assets in the container build? 
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
