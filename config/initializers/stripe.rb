ab3b6c1442bc1a10543bb56b662aad41a06d9b5f
Konstantin Gredeskoul <kigster@gmail.com>
2022-05-05 12:44:35 -0700

    Fixing rubocop complaint

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
-#stripe_config = YAML.load_file("#{Rails.root}/config/stripe.yml")
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY']
