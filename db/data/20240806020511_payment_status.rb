433498145a21176a3ab18034eeb986761ac4277a
Matt Levy <beingmattlevy@users.noreply.github.com>
2024-08-20 11:46:03 -0700

    fix migration

--- db/data/20240806020511_payment_status.rb
+++ db/data/20240806020511_payment_status.rb
@@ -2,7 +2,7 @@
 
 class PaymentStatus < ActiveRecord::Migration[7.1]
   def up
-    matrix = { 'N' => 'new', 'P' => 'in progress', 'R' => 'received', 'F' => 'refunded' }
+    matrix = { 'N' => 'new', 'P' => 'in_progress', 'R' => 'received', 'F' => 'refunded' }
     Payment.find_each do |p|
       p.status = matrix[p.old_status]
       p.save!
