45851a36d5aac6663de1a98bb8bd1ffef16096f5
Matt Levy <beingmattlevy@users.noreply.github.com>
2024-08-20 11:57:15 -0700

    cancel status migration for now

--- db/data/20240806020511_payment_status.rb
+++ db/data/20240806020511_payment_status.rb
@@ -2,13 +2,13 @@
 
 class PaymentStatus < ActiveRecord::Migration[7.1]
   def up
-    matrix = { 'N' => 'new', 'P' => 'in_progress', 'R' => 'received', 'F' => 'refunded' }
-    Payment.find_each do |p|
-      unless p.status == matrix[p.old_status]
-        p.status = matrix[p.old_status]
-        p.save!
-      end
-    end
+    # matrix = { 'N' => 'new', 'P' => 'in_progress', 'R' => 'received', 'F' => 'refunded' }
+    # Payment.find_each do |p|
+    #   unless p.status == matrix[p.old_status]
+    #     p.status = matrix[p.old_status]
+    #     p.save
+    #   end
+    # end
   end
 
   def down
