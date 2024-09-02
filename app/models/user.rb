edcbd8e866906ac291b7b46aefd5c2c26f74121f
Matt Levy <beingmattlevy@users.noreply.github.com>
2024-09-02 10:14:07 -0700

    add index to users authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,7 @@
 #
 # Indexes
 #
+#  index_users_on_authentication_token  (authentication_token)
 #  index_users_on_confirmation_token    (confirmation_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
