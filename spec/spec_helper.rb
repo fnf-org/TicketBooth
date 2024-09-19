7b9245c170e38ab9f7e2110a2cf1bc0937371b4d
Konstantin Gredeskoul <kigster@gmail.com>
2024-09-19 16:24:20 -0700

    Upgrading Ruby to 3.5.5, Rails to 7.1.4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+# Enable YJIT if we have it compiled in
+if defined?(RubyVM::YJIT) && RubyVM::YJIT.respond_to?(:enable)
+  RubyVM::YJIT.enable
+  puts '[ ✔ ] YJIT is enabled'
+else
+  warn '[ 𐄂 ] YJIT is not enabled'
+end
+
 ENV['RAILS_ENV'] = 'test'
 
 require 'simplecov'
