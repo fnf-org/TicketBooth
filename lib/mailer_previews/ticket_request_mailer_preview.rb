80582fb895d57594fcd42c5eb3a942e2fde12721
Matt Levy <beingmattlevy@users.noreply.github.com>
2024-09-20 13:26:59 -0700

    shorten

--- lib/mailer_previews/ticket_request_mailer_preview.rb
+++ lib/mailer_previews/ticket_request_mailer_preview.rb
@@ -19,19 +19,6 @@ The volunteer signup sheet: https://signup.app.fnf.org
 If you are looking for a great way to contribute and want to meet a lot of people along the way, <br>
 consider taking a shift as a Site Coordinator (SkC).
 <p>
-SC Sign up is here: https://forms.gle/jaM592Rc5fr6ZZH6A
-<p>
-WEATHER:
-<p>
-According to the weather forecast, it'll be in the mid to high 80s during the day, and low 60s to high 50s at night in Groveland, CA. <br>
-Spinning Wheel is a microclimate environment and often has slightly different weather. Be prepared! <br>
-We will continue to update the FC website with information as it comes our way.
-<p>
-ICE:
-<p>
-Unfortunately, we will not have ice for sale or a food truck on site. Please bring plenty of ice (freezing a gallon container of water is great). <br>
-We will try and make an ice run for everyone on Saturday around noon.
-<p>
 WEBSITE:
 <p>
 We will continue to update The Fall Classic event page with all of the information a raver needs,
