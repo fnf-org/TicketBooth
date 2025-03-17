9f34ef5f3e3599cd323b53007ee8a66e5eff23d1
Konstantin Gredeskoul <kigster@gmail.com>
2025-03-16 19:39:31 -0700

    Add comments to wrap_parameters

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -3,13 +3,17 @@
 # This file contains settings for ActionController::ParamsWrapper which
 # is enabled by default.
 
-# Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
+# @description Enable parameter wrapping for JSON. You can disable this by 
+#              setting :format to an empty array.
+#
+# @see https://edgeapi.rubyonrails.org/classes/ActionController/ParamsWrapper.html
 ActiveSupport.on_load(:action_controller) do
   wrap_parameters format: [:json]
 end
 
 PARAMS_WRAPPER_MUTEX = Mutex.new
 
+# Since Rails 8 options class is missing synchronize method for some reason
 ActionController::ParamsWrapper::Options.class_eval do
   def synchronize(&)
     PARAMS_WRAPPER_MUTEX.synchronize(&)
